a1e1ad930e7c1ee4a56a03580cce5744943dbbd4
eric <theitproinfo@gmail.com>
2023-01-31 22:01:21 -0500

    first commit

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = "Hello World! this is eric";
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+        }
+    }
+}
