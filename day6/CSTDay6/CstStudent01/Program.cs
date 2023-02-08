d0398722240b9d35ae23d60d4d463d1029e0e923
eric <theitproinfo@gmail.com>
2023-02-08 18:46:12 -0500

    day 6 commit

---
+++ day6/CSTDay6/CstStudent01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CstStudent01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student("Tom","Male",18,80,98,100);
+            st1.SayHello();
+
+            Console.ReadKey();
+
+        }
+    }
+}
