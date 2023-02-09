bb435b043540c0d7ec30edacddb5a1e1e6d91252
eric <theitproinfo@gmail.com>
2023-02-09 18:39:46 -0500

    day 7

---
+++ day7/CstDay7/CstDemo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CstDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Jacky",18,"001");
+
+           
+            cat.SayHello();
+            cat.Move();
+            cat.Speak();
+
+            //cat.Sleeping();
+
+            //Cat.Eat();
+            Console.ReadKey();
+          
+            Cat cat2=new Cat ("Tom",12,"007");
+
+            cat2.SayHello();
+            cat2.Move();
+            cat2.Speak();
+
+
+
+
+           
+        }
+    }
+}
